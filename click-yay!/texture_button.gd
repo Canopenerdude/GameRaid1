f2e6786d49c105692fa4a8a0cab152403804b42d
GregoryDevo <122557087+GregoryDevo@users.noreply.github.com>
2026-03-21 19:47:24 -0400

    The best one yet

--- click-yay!/texture_button.gd
+++ click-yay!/texture_button.gd
@@ -6,7 +6,8 @@ var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"../Next_button".disabled = true
+	$"../Next_button".visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,8 +15,14 @@ func _process(delta: float) -> void:
 	counter_display.text = str(count)
 	
 	if currentScene != "res://Yay_Klingon.tscn":
+		if count == 10:
+			$"../Next_button".disabled = false
+			$"../Next_button".visible = true
+	
+	if currentScene != "res://Yay_Klingon.tscn":
 		if count > 10:
 			count = 10
+	
 
 func _on_pressed() -> void:
 	$"../Alex_Sound".play()
@@ -36,5 +43,6 @@ func _on_reset_button_up() -> void:
 	get_tree().change_scene_to_file("res://Yay_Norse.tscn")
 
 
-func _on_texture_button_3_button_up() -> void:
-	pass # Replace with function body.
+#These Do the same thing
+func _on_texture_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://intro_scene.tscn")
