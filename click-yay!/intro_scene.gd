448cb8db309949adddb347108651837b8eb02c52
GregoryDevo <122557087+GregoryDevo@users.noreply.github.com>
2026-03-21 18:51:00 -0400

    Current Build

--- click-yay!/intro_scene.gd
+++ click-yay!/intro_scene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@onready var startingPlace = "res://Yay_Norse.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,19 +11,19 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_yay_button_button_up() -> void:
-	get_tree().change_scene_to_file("res://Yay_English.tscn")
+	get_tree().change_scene_to_file(startingPlace)
 
 func _on_yay_button_2_button_up() -> void:
-	get_tree().change_scene_to_file("res://Yay_English.tscn")
+	get_tree().change_scene_to_file(startingPlace)
 
 
 func _on_yay_button_3_button_up() -> void:
-	get_tree().change_scene_to_file("res://Yay_English.tscn")
+	get_tree().change_scene_to_file(startingPlace)
 
 
 func _on_alt_yay_pressed() -> void:
-	get_tree().change_scene_to_file("res://Yay_English.tscn")
+	get_tree().change_scene_to_file(startingPlace)
 
 
 func _on_alt_yay_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://Yay_English.tscn")
+	get_tree().change_scene_to_file(startingPlace)
--- click-yay!/texture_button.gd
+++ click-yay!/texture_button.gd
@@ -1,5 +1,8 @@
 extends TextureButton
 
+@onready var counter_display = $"../Label2"
+@onready var currentScene = get_tree().current_scene.scene_file_path
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,8 +10,23 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:	
+	counter_display.text = str(count)
+	
+	if currentScene != "res://Yay_Klingon.tscn":
+		if count > 10:
+			count = 10
 
 func _on_pressed() -> void:
 	$"../Alex_Sound".play()
+	count += 1
+
+func _on_next_button_pressed() -> void:
+	if currentScene == "res://Yay_Norse.tscn" and counter_display.text == "10":
+		get_tree().change_scene_to_file("res://Yay_Old English.tscn")
+	if currentScene == "res://Yay_Old English.tscn" and counter_display.text == "10":
+		get_tree().change_scene_to_file("res://Yay_Middle English.tscn")
+	if currentScene == "res://Yay_Middle English.tscn" and counter_display.text == "10":
+		get_tree().change_scene_to_file("res://Yay_English.tscn")
+	if currentScene == "res://Yay_English.tscn" and counter_display.text == "10":
+		get_tree().change_scene_to_file("res://Yay_Klingon.tscn")
