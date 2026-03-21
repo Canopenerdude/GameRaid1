a96eee3f98cddc0be5175c4ba777b85eb2b484d5
GregoryDevo <122557087+GregoryDevo@users.noreply.github.com>
2026-03-21 17:28:30 -0400

    Main Menu Added

---
+++ click-yay!/intro_scene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_yay_button_button_up() -> void:
+	get_tree().change_scene_to_file("res://Yay_Main.tscn")
+
+func _on_yay_button_2_button_up() -> void:
+	get_tree().change_scene_to_file("res://Yay_Main.tscn")
+
+
+func _on_yay_button_3_button_up() -> void:
+	get_tree().change_scene_to_file("res://Yay_Main.tscn")
